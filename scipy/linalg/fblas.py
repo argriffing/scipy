4c6384a243d7b96e72317c09bf7bd9a2497ecf7d
alex <argriffi@ncsu.edu>
2013-05-14 18:17:48 -0400

    put it back...

--- scipy/linalg/fblas.py
+++ scipy/linalg/fblas.py
@@ -3,7 +3,8 @@ This module is deprecated -- use scipy.linalg.blas instead
 """
 from __future__ import division, print_function, absolute_import
 
-from scipy.linalg._fblas import *
+#from scipy.linalg._fblas import *
+from _fblas import *
 import numpy as _np
 
 
